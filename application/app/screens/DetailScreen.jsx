01b58c5f78065532892c519664d4d7a4cff88bef
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-05-25 00:22:37 +0100

    debug rn

--- application/app/screens/DetailScreen.jsx
+++ application/app/screens/DetailScreen.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
 import axios from 'axios'
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor:'#fff'
     }
 });
  
@@ -12,10 +15,9 @@ class DetailScreen extends Component {
     
     constructor(props) {
         super(props);
-        
-    
         this.state = {
-            itemID: [],
+            itemID:this.props.route.params.itemID,
+            dataID: [],
             isLoading: false,
         };
     }
@@ -26,12 +28,9 @@ class DetailScreen extends Component {
     
     async getCoinByID() {
         try {
-            this.setState({itemID: this.props.route.params.itemID})
-            const {itemID} = this.state;
-            console.log(itemID);
-            const response = await axios.get('https://api.coincap.io/v2/assets'+itemID)
-            // console.log(response.data.data);
-            this.setState({data:response.data.data})
+            const response = await axios.get('https://api.coincap.io/v2/assets/'+this.state.itemID)
+            console.log(response.data.data);
+            this.setState({dataID:response.data.data})
         } catch (error) {
             console.error(error);
             this.setState({isLoading:true})
@@ -39,10 +38,29 @@ class DetailScreen extends Component {
     }
 
     render() { 
-        const {itemID} = this.state
+
+        const {dataID} = this.state
+
         return (
             <View style={styles.container}>
-                <Text>DetailScreen {itemID}</Text>
+                <ListItem style={{shadowColor: 'black',shadowOffset: {width: 0, height: 1},shadowOpacity: 0.2,elevation: 1}}>
+                    {/* <Avatar source={{uri: `https://assets.coincap.io/assets/icons/${dataID.symbol.toLowerCase()}@2x.png`}} /> */}
+                    <ListItem.Content style={{flex:5}}>
+                        <ListItem.Title>{dataID.name}</ListItem.Title>
+                        <ListItem.Subtitle>{dataID.symbol}</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <ListItem.Title style={{flex:5}}>
+                        ${ Number.parseFloat(dataID.priceUsd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </ListItem.Title>
+                    <ListItem.Title > 
+                        { dataID.changePercent24Hr>0 ? 
+                        <Text style={{color:'green'}}> 
+                            {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
+                        </Text>: <Text style={{color:'red'}}>
+                            {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
+                        </Text>}
+                    </ListItem.Title>
+                </ListItem>
             </View>
         );
     }
