e6edae596280d63f94bbbcbfc02f6d5fa27112fd
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-05-25 14:04:29 +0100

    detail screen data

--- application/app/screens/DetailScreen.jsx
+++ application/app/screens/DetailScreen.jsx
@@ -7,7 +7,8 @@ import axios from 'axios'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'#fff'
+        backgroundColor:'#fff',
+        padding:10
     }
 });
  
@@ -16,7 +17,7 @@ class DetailScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            itemID:this.props.route.params.itemID,
+            // itemID:this.props.route.params.itemID,
             dataID: [],
             isLoading: false,
         };
@@ -28,7 +29,8 @@ class DetailScreen extends Component {
     
     async getCoinByID() {
         try {
-            const response = await axios.get('https://api.coincap.io/v2/assets/'+this.state.itemID)
+            // const response = await axios.get('https://api.coincap.io/v2/assets/'+this.state.itemID)
+            const response = await axios.get('https://api.coincap.io/v2/assets/bitcoin')
             console.log(response.data.data);
             this.setState({dataID:response.data.data})
         } catch (error) {
@@ -45,25 +47,45 @@ class DetailScreen extends Component {
             <View style={styles.container}>
                 <ListItem style={{shadowColor: 'black',shadowOffset: {width: 0, height: 1},shadowOpacity: 0.2,elevation: 1}}>
                     {/* <Avatar source={{uri: `https://assets.coincap.io/assets/icons/${dataID.symbol.toLowerCase()}@2x.png`}} /> */}
-                    <ListItem.Content style={{flex:5}}>
-                        <ListItem.Title>{dataID.name}</ListItem.Title>
-                        <ListItem.Subtitle>{dataID.symbol}</ListItem.Subtitle>
-                    </ListItem.Content>
-                    <ListItem.Title style={{flex:5}}>
-                        ${ Number.parseFloat(dataID.priceUsd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                    </ListItem.Title>
-                    <ListItem.Title > 
-                        { dataID.changePercent24Hr>0 ? 
-                        <Text style={{color:'green'}}> 
-                            {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
-                        </Text>: <Text style={{color:'red'}}>
-                            {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
-                        </Text>}
-                    </ListItem.Title>
+        
                 </ListItem>
+                <View >
+                    <View style={{flexDirection:'row'}}>
+                        <Avatar source={{uri: `https://assets.coincap.io/assets/icons/${dataID.symbol}@2x.png`}} />
+                        <View style={{marginHorizontal:30}}>
+                            <ListItem.Title style={{fontWeight:'bold'}}>{dataID.name}({dataID.symbol})</ListItem.Title>
+                            <Text >{((new Date().getDate() > 9) ? new Date().getDate() : ('0' + new Date().getDate())) + ' ' +((new Date().getMonth() > 8) ? (new Date().getMonth() + 1) : ('0' + (new Date().getMonth() + 1))) + ' ' + new Date().getFullYear()}</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection:'row', justifyContent:'space-between', marginTop:20}}>
+                        <View >
+                            <ListItem.Subtitle>
+                                HIGH <ListItem.Title> ${ Number.parseFloat(dataID.priceUsd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</ListItem.Title>
+                            </ListItem.Subtitle> 
+                            <ListItem.Subtitle style={{marginVertical:20}}>
+                                LOW <ListItem.Title> ${ Number.parseFloat(dataID.priceUsd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</ListItem.Title>
+                            </ListItem.Subtitle>                     
+                        </View>
+                        <View >
+                            <ListItem.Subtitle>
+                                AVERAGE <ListItem.Title> ${ Number.parseFloat(dataID.priceUsd).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</ListItem.Title>
+                            </ListItem.Subtitle> 
+                            <ListItem.Subtitle style={{marginVertical:20}}>
+                                CHANGE <ListItem.Title> 
+                                 { dataID.changePercent24Hr>0 ? 
+                                    <Text style={{color:'green'}}> 
+                                        {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
+                                    </Text>: <Text style={{color:'red'}}>
+                                        {Number.parseFloat(dataID.changePercent24Hr).toFixed(2)}% 
+                                    </Text>}
+                                </ListItem.Title>
+                            </ListItem.Subtitle>                     
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
 }
- 
+
 export default DetailScreen;
