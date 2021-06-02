cb4c11cd07c3b350bc3537bce8f6d6122efad1da
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-06-02 12:03:20 +0100

    deploy app in heroku

--- api-service-achat/src/app/app.ts
+++ api-service-achat/src/app/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 const app = express();
-import db from './config/db';
-import userRouter from './routers/UsersRouter';
-import walletRouter from './routers/WalletRouter';
+import db from '../config/db';
+import userRouter from '../routers/UsersRouter';
+import walletRouter from '../routers/WalletRouter';
 import cors from 'cors'
 
 app.use(express.json());
@@ -10,12 +10,13 @@ app.use(cors());
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/wallet', walletRouter)
+const port = process.env.PORT || 4000
 
-app.listen( 4000,()=>{
+app.listen( port,()=>{
     console.log('app is runing in port 4000');
     db.sync().then(() => {
         console.log('db success');
     }).catch((err:any)=>{
-        console.log(err.message);
+        console.log(err.message+'errrrr');
     })
 })
--- api-service-achat/src/config/db.ts
+++ api-service-achat/src/config/db.ts
@@ -1,5 +1,23 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('postgres://postgres:0909@localhost:5432/cryptodb');
+// const db = new Sequelize(`postgres://postgres:0909@localhost:5432/cryptodb`);
+
+// const db = new Sequelize('postgres://chuxbitcdruaui:dc1a5858e88a696b0c2a7964255166737e8c7e414e04f59b9def92394ec076c4@ec2-52-4-171-132.compute-1.amazonaws.com:5432/d4cthme49l1m62');
+const db = new Sequelize('d4cthme49l1m62','chuxbitcdruaui','dc1a5858e88a696b0c2a7964255166737e8c7e414e04f59b9def92394ec076c4',{
+    "host": 'ec2-52-4-171-132.compute-1.amazonaws.com',
+    "port": 5432,
+    "dialect": "postgres",
+    "ssl": true,
+    "protocol": "postgres",
+
+    "logging": true, 
+    "dialectOptions": { 
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false 
+        }
+    }
+})
+
 
 export default db;
